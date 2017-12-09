fe51c8832f4166f89832761f6123db71da734bb4
rafaeel16 <rafaeel.16@gmail.com>
2017-12-09 19:43:38 -0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    $("#header").load("pages/header.html");
+    $("#content").load("pages/inicio.html");
+});
+
+function redirectPage(page){
+    if (page == 1) {
+        $("#content").html('');
+        $("#content").load("pages/inicio.html");
+        $('nav li').removeClass('active');
+        $($this).addClass('active');
+    }
+    if (page == 2) {
+        $("#content").html('');
+        $("#content").load("pages/cadastrarOcorrencia.html");
+    }
+    if (page == 3) {
+        $("#content").html('');
+        $("#content").load("pages/listarOcorrencias.html");
+    }
+    if (page == 4) {
+        $("#content").html('');
+        $("#content").load("pages/mapa.html");
+    }
+}
+
+function cadastrarOcorrencia(){
+
+}
+
+function listarOcorrencia(){
+         //Ajax para preencher o Select dos Counters Heroes
+         $.ajax({
+            url: "http://techsaferj.com.br/znoeste/api/public/Ocorrencia",
+            type: "GET",
+            dataType: "JSON",
+            success: function (response) {
+                console.log(response);
+                // var optionHeroi = "<option value='0'>--Select Hero--</option>";
+                // $.each(response, function (i, item) {
+                //     optionHeroi += "<option value='" + item.id + "'>" + item.name + "</option>";
+                // }); //Fim Each
+   
+            },
+            error: function (e) {
+                console.log("Erro: " + e);
+            }
+   
+        });
+}
+
+function cadastrarUsuario(){
+
+}
+
+function editarUsuario(){
+
+}
+
+function login(){
+
+}
+
+function redefinirSenha(){
+
+}
+
+function filtrarOcorrencias(){
+
+}
