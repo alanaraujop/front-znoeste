60cdbb72d78d2f242057b318faf4463cab07939f
rafaeel16 <rafaeel.16@gmail.com>
2017-12-10 23:29:05 -0200

    Login e Logout

--- js/app.js
+++ js/app.js
@@ -1,23 +1,37 @@
-$(document).ready(function(){
-    $("#header").load("pages/header.html");
-    $("#content").load("pages/inicio.html");
+$(document).ready(function () {
+    var user = localStorage.getItem("email");
+    var id = localStorage.getItem("id");
+    var nivel_acesso = localStorage.getItem("nivel_acesso");
+
+    if (user != "" && user != null && nivel_acesso == 1) {
+        $("#content").html('');
+        redirectPage(1, null);
+    }
+    if (user == "" || user == null) {
+        $("#content").html('');
+        $("#content").load("pages/login.html");
+    }
 });
 
-function redirectPage(page, el){
+//Redirecionamento das páginas SPA
+function redirectPage(page, el) {
     if (page == 1) {
         $("#content").html('');
+        $("#header").load("pages/header.html");
         $("#content").load("pages/inicio.html");
         $('nav li').removeClass('active');
-        $(el).addClass('active');
+        $('nav li:nth-child(1)').addClass('active');
     }
     if (page == 2) {
         $("#content").html('');
+        $("#header").load("pages/header.html");
         $("#content").load("pages/cadastrarOcorrencia.html");
         $('nav li').removeClass('active');
         $(el).addClass('active');
     }
     if (page == 3) {
         $("#content").html('');
+        $("#header").load("pages/header.html");
         $("#content").load("pages/listarOcorrencias.html");
         $('nav li').removeClass('active');
         $(el).addClass('active');
@@ -25,6 +39,7 @@ function redirectPage(page, el){
     }
 }
 
+//Cadastro de Ocorrencias da página de ocorrencias
 function cadastrarOcorrencia() {
     var usuario_id = 1; //trazer o id do cookie
     var tipoOcorrencia = $("#ddlTipoOcorrencia").val();
@@ -36,12 +51,12 @@ function cadastrarOcorrencia() {
     $.ajax({
         type: "POST",
         data: {
-            "tipo_ocorrencia_id":tipoOcorrencia,
+            "tipo_ocorrencia_id": tipoOcorrencia,
             "data": data,
             "bairro_id": bairro,
             "endereco": endereco,
             "descricao": descricao,
-            "usuario_id":usuario_id
+            "usuario_id": usuario_id
         },
         url: "http://techsaferj.com.br/znoeste/api/public/Ocorrencia",
         success: function (data) {
@@ -60,39 +75,50 @@ function cadastrarOcorrencia() {
 
 }
 
-function listarOcorrencia(){
-         //Ajax para preencher o Select dos Counters Heroes
-         $.ajax({
-            url: "http://techsaferj.com.br/znoeste/api/public/Ocorrencia",
-            type: "GET",
-            dataType: "JSON",
-            success: function (response) {
-
-   
-            },
-            error: function (e) {
-                console.log("Erro: " + e);
-            }
-   
-        });
-}
-
-function cadastrarUsuario(){
+function cadastrarUsuario() {
 
 }
 
-function editarUsuario(){
+function editarUsuario() {
 
 }
 
-function login(){
+function login() {
+    localStorage.clear();
+    var email = $("#email").val();
+    var senha = $("#senha").val();
+
+    $.ajax({
+        type: "POST",
+        data: {
+            "email": email,
+            "senha": senha,
+        },
+        url: "http://techsaferj.com.br/znoeste/api/public/Login",
+        success: function (response) {
+            localStorage.setItem("id", response.id);
+            localStorage.setItem("email", response.email);
+            localStorage.setItem("nivel_acesso", response.nivel_acesso);
+            redirectPage(1);
+        },
+        error: function (e) {
+            console.log("Erro: " + e);
+        }
+    });
+}
+
+function logout() {
+    localStorage.clear();
+    $("#header").html('');
+    $("#content").html('');
+    $("#content").load("pages/login.html");
 
 }
 
-function redefinirSenha(){
+function redefinirSenha() {
 
 }
 
-function filtrarOcorrencias(){
+function filtrarOcorrencias() {
 
 }
